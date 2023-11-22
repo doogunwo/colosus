ba3e6e1ab6cdd3d97287793c22cbbfc012382a56
doogunwo <dgw0601@naver.com>
2023-11-22 17:05:45 +0000

    암호화 된다

--- src/main.c
+++ src/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
+#include "../bin/crypto.h"
 
 #define MAX_FILE_SIZE 1024
 
+int main(int argc, char *argv[]) {
 
-int main(){
-    
-   
+    const char *inputFileName = "data/t1.txt";  // 세미콜론 추가
 
+    const char *publicKeyFileName = "wallet/public.pem";
+    const char *privateKeyFileName = "wallet/private.pem";
+
+    const char *encryptedFileName = "data/encrypted.bin";
+    const char *decryptedFileName = "data/decrypted.txt";
+
+    if (strcmp(argv[1], "enc") == 0) {  // 문자열 비교 수정
+        encryptFile(inputFileName, publicKeyFileName, encryptedFileName);
+        printf("File encrypted successfully.\n");
+    }
+    if (strcmp(argv[1], "dec") == 0) {  // 문자열 비교 수정
+        decryptFile(encryptedFileName, privateKeyFileName, decryptedFileName);
+        printf("File decrypted successfully.\n");
+    }
+
+    return 0;
 }
+//gcc main.c ../bin/crypto.c -o colosus -lssl -lcrypto
